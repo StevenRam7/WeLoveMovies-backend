c4d4527c3b2c208c1beb67519b628bf98c59276b
Steven <svramirez20@gmail.com>
2021-11-26 22:24:29 -0500

    movies functions complete

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -1,9 +1,27 @@
-const service = require("./movies.service")
+const service = require("./movies.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+
+async function movieExists(req, res, next) {
+  const film = await service.read(req.params.movieId);
+  if (film) {
+    res.locals.film = film;
+    return next()
+  }
+  next({
+    status: 404,
+    message: "Movie cannot be found."
+  })
+} 
 
 async function list(req, res, next) {
-    res.json({ data: await service.list() })
+    res.json({ data: await service.list(req.query.is_showing) })
+}
+
+async function read(req, res, next) {
+  res.json({ data: res.locals.film })
 }
 
 module.exports = {
-  list
+  list,
+  read: [asyncErrorBoundary(movieExists), asyncErrorBoundary(read)]
 }
--- src/movies/movies.service.js
+++ src/movies/movies.service.js
@@ -1,10 +1,27 @@
 const knex = require("../db/connection");
 
-function list() {
-  return knex("movies")
-  .select("*")
+function list(isShowing) {
+  if (isShowing) {
+    return knex("movies as m")
+    .join ("movies_theaters as mt", "mt.movie_id", "m.movie_id")
+    .select("m.*")
+    .where({ "mt.is_showing": true })
+    .distinct()
+  }
+  else {
+    return knex("movies")
+    .select("*")
+  }
  }
 
+function read(movieId) {
+  return knex("movies")
+  .select("*")
+  .where({ "movie_id": movieId })
+  .then((result) => result[0])
+}
+
 module.exports = {
-  list
+  list,
+  read
  }
