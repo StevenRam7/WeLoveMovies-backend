2c9f80fe0eb2ec3f65067c376ef5a6339354d8c8
Steven <svramirez20@gmail.com>
2021-12-04 20:38:30 -0500

    review update fixed

--- src/reviews/reviews.controller.js
+++ src/reviews/reviews.controller.js
@@ -1,6 +1,18 @@
 const service = require("./reviews.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+const mapProperties = require("../utils/map-properties");
 
+const config = {
+    preferred_name: "critic.preferred_name",
+    surname: "critic.surname",
+    organization_name: "critic.organization_name"
+};
+const addCritic= mapProperties(config);
+const addCriticArray= (array)=>{
+    return array.map((item)=>addCritic(item));
+};
+
+//middleware
 async function reviewExists(req, res, next) {
   const reviewX = await service.read(req.params.reviewId);
   if (reviewX) {
@@ -11,13 +23,27 @@ async function reviewExists(req, res, next) {
     status: 404,
     message: "Review cannot be found."
   })
-} 
+}
 
+function hasContent(req, res, next){
+    console.log(req.body.data);
+    const {data: {content}={}}= req.body;
+    console.log(content);
+    if(content){
+        res.locals.content=content;
+        return next();
+    }else{
+        next({status: 400, message:`Must include content`});
+    }
+}
+
+//handlers
 async function update(req, res, next) {
-  await service.update(req.body.data, req.params.reviewId)
-  res.json({ data: await 
-            service.read(req.params.reviewId)
-             })
+    const newReview = req.body.data;
+    const { review_id } = res.locals.reviewX;
+    await service.update(review_id, newReview);
+    const data = await service.readUpdatedReview(review_id)
+    res.json({ data });
 }
 
 async function destroy(req, res, next) {
--- src/reviews/reviews.service.js
+++ src/reviews/reviews.service.js
@@ -1,18 +1,41 @@
 const knex = require("../db/connection");
+const mapProperties = require("../utils/map-properties");
 
-function update(updatedReview, reviewId) {
-   console.log(updatedReview)
+const config = {
+    critic_id: "critic.critic_id",
+    preferred_name: "critic.preferred_name",
+    surname: "critic.surname",
+    organization_name: "critic.organization_name"
+};
+const addCritic= mapProperties(config);
+const addCriticArray= (array)=>{
+    return array.map((item)=>addCritic(item));
+};
+
+function update(review_id, updatedReview) {
   return knex("reviews")
-  .update({ ...updatedReview })
-  
+    .select("*")
+    .where({ review_id })
+    .update(updatedReview, "*");
+}
+
+function readUpdatedReview(review_id) {
+  return knex("reviews as r")
+    .join("critics as c", "r.critic_id", "c.critic_id")
+    .select("*")
+    .where({ review_id })
+    .first()
+    .then((result) => {
+      const updatedReview = addCritic(result);
+      return {...updatedReview, critic_id: updatedReview.critic.critic_id}
+    });
 }
 
 function read(reviewId) {
    return knex("reviews as r")
-  .join("critics as c", "r.critic_id", "c.critic_id")
   .select("*")
   .where({ review_id: reviewId })
-  //.then((result) => result[0])
+  .first()
 }
 
 function destroy(reviewId){
@@ -24,5 +47,6 @@ function destroy(reviewId){
 module.exports = {
     read,
    update,
+  readUpdatedReview,
    delete: destroy
  }
